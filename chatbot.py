9cdcb9dd152412d7b8278f010152a912009161da
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-11-17 12:35:59 -0300

    analytics now get saved to firestore

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
 import os
 import pinecone
 import streamlit as st
-import streamlit_analytics as st_analytics
 from dotenv import load_dotenv
+from google.cloud import firestore
+import time
 
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -24,7 +25,26 @@ PINECONE_ENV = os.getenv("PINECONE_ENV")
 load_dotenv()
 set_llm_cache(InMemoryCache())
 
-llm = ChatOpenAI(model='gpt-3.5-turbo', max_tokens=1000)
+
+# Instanciar llm
+@st.cache_resource(show_spinner=False)
+def get_llm():
+    llm = ChatOpenAI(model='gpt-3.5-turbo', max_tokens=1000)
+    return llm
+
+
+# Conectar con firestore
+@st.cache_resource
+def db_connection():
+    db = firestore.Client.from_service_account_json("firestore-key.json")
+    return db
+
+
+# Total de chats
+def get_chats_len():
+    chats_ref = db_connection().collection('chats').get()
+    return len(chats_ref)
+
 
 # Listado de nombres de documentos
 @st.cache_data
@@ -35,6 +55,7 @@ def get_doc_names():
     return file_names
 
 
+# Carga y split de textos
 @st.cache_resource
 def load_and_split_docs():
     # Carga de documentos
@@ -54,9 +75,9 @@ def load_and_split_docs():
     return texts
 
 
-# Embedding
-## Importar vectorstore
-@st.cache_resource
+# Embeddings
+# Importar vectorstore
+@st.cache_resource(show_spinner=False)
 def get_vectorstore():
     embeddings = OpenAIEmbeddings()
     pinecone.init(
@@ -83,19 +104,45 @@ def do_embedding(text_chunks):
     return vectorstore
 
 
+# Generacion de respuesta
 def answer_question(vectorstore, question):
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k":2})
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, max_tokens_limit=2000, verbose=True)
+    chain = ConversationalRetrievalChain.from_llm(llm=get_llm(), retriever=retriever, max_tokens_limit=2000, verbose=True)
 
     with get_openai_callback() as cb:
         result = chain({"question": question, "chat_history": st.session_state.chat_history})
+        with st.expander('tokens'):
+            cb
         print(result)
         tokens_used = cb.total_tokens
         print(cb)
 
     st.session_state.chat_history = [(question, result['answer'])]
-
     return result['answer'], tokens_used
+
+
+# Registrar datos en la base de datos 
+def add_to_db(question, answer, tokens, time_to_answer):
+    chat_id = st.session_state.chat_id
+    db = db_connection()
+
+    chats_ref = db.collection('chats')
+    chat_doc_ref = chats_ref.document(chat_id)
+
+    # Revisar si documento con chat_id existe
+    chat_doc = chat_doc_ref.get()
+    if not chat_doc.exists:
+        # Crearlo en caso de que no exista
+        chat_doc_ref.set({})
+
+    # Agregar pregunta y respuesta a sub coleccion messages
+    messages_ref = chat_doc_ref.collection('messages')
+    messages_ref.add({
+        'question': question,
+        'answer': answer,
+        'tokens': tokens,
+        'time_to_answer': time_to_answer
+    })
 
 
 def main():
@@ -104,11 +151,14 @@ def main():
         page_icon="🤖",
     )
 
-    vectorstore = get_vectorstore()
     file_names = get_doc_names()
+    col1,col2,col3 = st.columns([3,0.5,0.5])
 
-    st.title('UNAP Chatbot 🤖📖')
-    st.write('Chat capaz de responder preguntas relacionadas a reglamentos y documentos relacionados con la universidad Arturo Prat. Actualmente es consciente de', len(file_names), 'documentos.')
+    st.title('🤖 UNAP Chatbot 📖')
+    st.write('Chat capaz de responder preguntas relacionadas a reglamentos y documentos relacionados con la universidad Arturo Prat. Actualmente es consciente de', 
+             len(file_names), 
+             'documentos.'
+             )
     
     with st.expander('Listado de documentos'):
         st.write(file_names)
@@ -118,17 +168,20 @@ def main():
         st.session_state.messages = [{"role": "assistant", "content": "¡Hola! ¿Como te puedo ayudar?"}]
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
-    if "feedback_given" not in st.session_state:
-        st.session_state.feedback_given = False
+    if "ask_feedback" not in st.session_state:
+        st.session_state.ask_feedback = True
+    if "chat_id" not in st.session_state:
+        st.session_state.chat_id = str(get_chats_len() + 1)
 
-    st_analytics.start_tracking()
     # Mantener historial en caso de rerun de app
     for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
+        with st.chat_message(message['role']):
             st.markdown(message["content"])
 
     # User input
-    if prompt := st.chat_input("Escribe tu pregunta..."):
+    prompt = st.chat_input("Escribe tu pregunta...")
+
+    if prompt:
         # Agregar input de usuario al historial
         st.session_state.messages.append({"role": "user", "content": prompt})
         # Mostrar input en su contenedor
@@ -137,23 +190,23 @@ def main():
         # Mostrar respuesta del LLM
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
+            start = time.time()
             with st.spinner('Generando respuesta...'):
-                full_response, tokens = answer_question(vectorstore=vectorstore, question=prompt)
+                full_response, tokens = answer_question(vectorstore=get_vectorstore(), question=prompt)
             message_placeholder.markdown(f'{full_response} *({tokens})*')
-            col1,col2,col3 = st.columns([3,0.5,0.5])
-            if not st.session_state.feedback_given:
+            end = time.time()
+            if st.session_state.ask_feedback:
                 with col1:
                     st.markdown('*¿Que te parecio la velocidad de la respuesta?*')
                 with col2:
-                    st.button(":thumbsup:")
+                    st.button(":thumbsup:", key='like')
                 with col3:
-                    st.button(":thumbsdown:")
-            else:
-                with col1:
-                    st.markdown('*Gracias por el feedback!*')
+                    st.button(":thumbsdown:", key='dislike')
         # Agregar respuesta del LLM al historial
         st.session_state.messages.append({"role": "assistant", "content": f'{full_response} *({tokens})*'})
-    st_analytics.stop_tracking()
+
+        add_to_db(question=prompt, answer=full_response, tokens=tokens, time_to_answer=end-start)
+
 
 if __name__ == '__main__':
     main()
