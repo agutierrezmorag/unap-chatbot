cd1fa4ed48a14841fdcbb916a2d0d59a3340f1b3
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2023-11-17 05:38:42 -0300

    added analytics and feedback buttons

--- chatbot.py
+++ chatbot.py
@@ -1,31 +1,30 @@
 import os
 import pinecone
 import streamlit as st
+import streamlit_analytics as st_analytics
 from dotenv import load_dotenv
 
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
 
-from langchain.llms import HuggingFaceHub
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chains.question_answering import load_qa_chain
 
 from langchain.callbacks import get_openai_callback
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferWindowMemory
+from langchain.globals import set_llm_cache
+from langchain.cache import InMemoryCache
 
 # API keys
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
-HUGGINGFACEHUB_API_TOKEN = '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'
-from langchain.globals import set_llm_cache
-from langchain.cache import InMemoryCache
+
+load_dotenv()
 set_llm_cache(InMemoryCache())
 
-llm = ChatOpenAI(model='gpt-3.5-turbo')
+llm = ChatOpenAI(model='gpt-3.5-turbo', max_tokens=1000)
 
 # Listado de nombres de documentos
 @st.cache_data
@@ -50,7 +49,7 @@ def load_and_split_docs():
         chunk_size=2048,
         chunk_overlap=128,
         length_function=len
-        )
+    )
     texts = text_splitter.split_documents(raw_text_files)
     return texts
 
@@ -86,11 +85,11 @@ def do_embedding(text_chunks):
 
 def answer_question(vectorstore, question):
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k":2})
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, max_tokens_limit=2000, return_source_documents=True)
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, max_tokens_limit=2000, verbose=True)
 
     with get_openai_callback() as cb:
         result = chain({"question": question, "chat_history": st.session_state.chat_history})
-        print(result['source_documents'])
+        print(result)
         tokens_used = cb.total_tokens
         print(cb)
 
@@ -100,7 +99,6 @@ def answer_question(vectorstore, question):
 
 
 def main():
-    load_dotenv()
     st.set_page_config(
         page_title="UNAP Chatbot 📖",
         page_icon="🤖",
@@ -109,20 +107,21 @@ def main():
     vectorstore = get_vectorstore()
     file_names = get_doc_names()
 
-    with st.sidebar:
-        if st.button('Crear embedding 📖'):
-            do_embedding(load_and_split_docs())
-        st.write(file_names)
-
     st.title('UNAP Chatbot 🤖📖')
     st.write('Chat capaz de responder preguntas relacionadas a reglamentos y documentos relacionados con la universidad Arturo Prat. Actualmente es consciente de', len(file_names), 'documentos.')
     
+    with st.expander('Listado de documentos'):
+        st.write(file_names)
+
     # # Inicializacion historial de chat
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        st.session_state.messages = [{"role": "assistant", "content": "¡Hola! ¿Como te puedo ayudar?"}]
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
+    if "feedback_given" not in st.session_state:
+        st.session_state.feedback_given = False
 
+    st_analytics.start_tracking()
     # Mantener historial en caso de rerun de app
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -130,7 +129,7 @@ def main():
 
     # User input
     if prompt := st.chat_input("Escribe tu pregunta..."):
-        # Agregar input de usuario al hisotrial
+        # Agregar input de usuario al historial
         st.session_state.messages.append({"role": "user", "content": prompt})
         # Mostrar input en su contenedor
         with st.chat_message("user"):
@@ -140,10 +139,21 @@ def main():
             message_placeholder = st.empty()
             with st.spinner('Generando respuesta...'):
                 full_response, tokens = answer_question(vectorstore=vectorstore, question=prompt)
-            message_placeholder.markdown(f'{full_response} **({tokens})**')
+            message_placeholder.markdown(f'{full_response} *({tokens})*')
+            col1,col2,col3 = st.columns([3,0.5,0.5])
+            if not st.session_state.feedback_given:
+                with col1:
+                    st.markdown('*¿Que te parecio la velocidad de la respuesta?*')
+                with col2:
+                    st.button(":thumbsup:")
+                with col3:
+                    st.button(":thumbsdown:")
+            else:
+                with col1:
+                    st.markdown('*Gracias por el feedback!*')
         # Agregar respuesta del LLM al historial
-        st.session_state.messages.append({"role": "assistant", "content": f'{full_response} {tokens}'})
-    
+        st.session_state.messages.append({"role": "assistant", "content": f'{full_response} *({tokens})*'})
+    st_analytics.stop_tracking()
 
 if __name__ == '__main__':
     main()
