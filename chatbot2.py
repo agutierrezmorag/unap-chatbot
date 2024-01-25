5cfb2fd625a2a4f80a8cb40090b28d63a4661045
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-01-24 21:41:19 -0300

    deleted token tracking, will do another way

--- chatbot2.py
+++ chatbot2.py
@@ -18,7 +18,6 @@ from langsmith import Client
 from st_pages import show_pages_from_config
 from streamlit_feedback import streamlit_feedback
 
-from utils.callbacks import TokenUsageTrackingCallbackHandler
 from documents_manager import get_repo_documents
 from utils import config
 
@@ -46,13 +45,9 @@ def get_llm():
     llm = ChatOpenAI(
         model="gpt-3.5-turbo-1106",
         openai_api_key=config.OPENAI_API_KEY,
+        temperature=0.7,
         max_tokens=1000,
         streaming=True,
-        callbacks=[
-            TokenUsageTrackingCallbackHandler(
-                model_name="gpt-3.5-turbo-1106", session_state=st.session_state.cost
-            )
-        ],
     )
     return llm
 
@@ -184,8 +179,6 @@ if __name__ == "__main__":
     # Inicializacion de variables de estado
     if "session_id" not in st.session_state:
         st.session_state.session_id = str(uuid.uuid4())
-    if "cost" not in st.session_state:
-        st.session_state.cost = {}
     if "run_id" not in st.session_state:
         st.session_state.run_id = ""
     if "memory" not in st.session_state:
