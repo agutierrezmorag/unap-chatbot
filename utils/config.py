b88dedf22eea13fde620ccf907081e2ff3c88fa7
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-02-03 00:42:10 -0300

    Add PINECONE_API_KEY to environment variables

--- utils/config.py
+++ utils/config.py
@@ -31,6 +31,8 @@ CHAT_ENVIRONMENT = st.secrets.langsmith.chat_environment
 # Tavily data
 TAVILY_API_KEY = st.secrets.tavily.api_key
 
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
 os.environ["LANGCHAIN_TRACING_V2"] = LANGCHAIN_TRACING_V2
 os.environ["LANGCHAIN_ENDPOINT"] = LANGCHAIN_ENDPOINT
 os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
