db94d3b21ddff9e2ef4aea688399b3838c4f28f7
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-02-08 15:02:35 -0300

    Refactor Wikipedia page loading and uploading

--- doc_manager/wikipedia_management.py
+++ doc_manager/wikipedia_management.py
@@ -1,16 +1,12 @@
-import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import WikipediaLoader
 from termcolor import cprint
 
 from doc_manager.pinecone_management import (
-    check_if_index_exists,
-    delete_namespace,
-    get_vectorstore,
+    split_and_load_documents_to_vectorstore,
 )
 
 
-def load_wikipedia_page_to_vectorstore():
+def upload_wikipedia_page() -> None:
     """
     Realiza el scraping de una página de Wikipedia, transforma los documentos, los limpia, los divide y los añade a un vector store.
 
@@ -21,36 +17,15 @@ def load_wikipedia_page_to_vectorstore():
         bool: Verdadero si los documentos se añadieron con éxito al vector store, Falso en caso contrario.
     """
     # Scrapping de la pagina de wikipedia
-    docs = WikipediaLoader(
+    loader = WikipediaLoader(
         query="Universidad Arturo Prat",
         lang="es",
         load_max_docs=1,
         load_all_available_meta=True,
         doc_content_chars_max=20000,
-    ).load()
+    )
+    docs = loader.load()
 
-    # Split de los documentos
-    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = splitter.split_documents(docs)
+    cprint(f"Cargados {len(docs)} documentos desde Wikipedia", "yellow")
 
-    try:
-        check_if_index_exists()
-
-        # Para evitar documentos duplicados, se elimina el namespace si es que existe
-        delete_namespace("Wikipedia")
-
-        # Recuperar index
-        vectorstore = get_vectorstore("Wikipedia")
-        # Añadir documentos
-        vectorstore.add_documents(documents=splits)
-        cprint("Documentos añadidos al namespace 'Wikipedia'", "green")
-
-        st.toast("Contenido añadido exitosamente.", icon="✅")
-    except Exception as e:
-        cprint(
-            f"Hubo un error al intentar añadir el contenido de Wikipedia al vector store: {e}",
-            "red",
-        )
-        st.toast(
-            "Hubo un error al intentar añadir el contenido de Wikipedia.", icon="❌"
-        )
+    split_and_load_documents_to_vectorstore(docs, "Wikipedia")
