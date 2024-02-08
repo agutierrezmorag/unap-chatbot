8423dfcde926e41268fa660eb5f75b865fecaef8
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-02-08 15:49:30 -0300

    refactored code and added type hints

--- doc_manager/github_management.py
+++ doc_manager/github_management.py
@@ -1,8 +1,10 @@
 import time
+from typing import List
 
 import streamlit as st
-from github import Auth, Github, GithubException
+from github import Auth, Github, GithubException, Repository
 from langchain_community.document_loaders import GitLoader
+from streamlit.delta_generator import DeltaGenerator
 from termcolor import cprint
 
 from doc_manager.pinecone_management import split_and_load_documents_to_vectorstore
@@ -10,7 +12,7 @@ from utils import config
 
 
 @st.cache_resource(show_spinner=False)
-def get_repo():
+def get_repo() -> Repository.Repository:
     """
     Recupera el objeto del repositorio de GitHub basado en el token de acceso proporcionado, el propietario del repositorio y el nombre del repositorio.
 
@@ -29,7 +31,7 @@ def get_repo():
 @st.cache_resource(
     ttl=60 * 60 * 24, show_spinner="Recuperando listado de documentos..."
 )
-def get_repo_documents():
+def get_repo_documents() -> List[str]:
     """
     Recupera los documentos del repositorio.
 
@@ -47,7 +49,9 @@ def get_repo_documents():
         return []
 
 
-def delete_repo_doc(file_path, commit_message="Eliminar archivo a través de Streamlit"):
+def delete_repo_doc(
+    file_path: str, commit_message: str = "Eliminar archivo a través de Streamlit"
+) -> bool:
     """
     Elimina un archivo del repositorio.
 
@@ -75,62 +79,59 @@ def delete_repo_doc(file_path, commit_message="Eliminar archivo a través de Str
         return False
 
 
-def add_files_to_repo(file_list, container):
+def add_files_to_repo(
+    file_list: List[str], container: DeltaGenerator, progress_bar: DeltaGenerator
+) -> None:
     """
-    Añade archivos a un repositorio.
+    Añade una lista de archivos a un repositorio de GitHub.
+
+    Esta función recorre una lista de archivos, verifica si cada archivo ya existe en el repositorio y, si no existe, lo añade al repositorio.
+    La función actualiza una barra de progreso en cada iteración del bucle.
 
     Args:
-        file_list (list): Una lista de archivos a añadir.
-        container (streamlit.delta_generator.DeltaGenerator): Un contenedor de Streamlit para mostrar mensajes.
+        file_list (List[str]): Una lista de archivos para añadir al repositorio.
+        container (DeltaGenerator): Un contenedor de Streamlit para mostrar mensajes.
+        progress_bar (DeltaGenerator): Un objeto de barra de progreso de Streamlit para mostrar el progreso de la adición de archivos.
+
+    Returns:
+        None
     """
     repo = get_repo()
 
-    for uploaded_file in file_list:
+    progress_bar.progress(0)
+    for i, uploaded_file in enumerate(file_list):
         content = uploaded_file.read().decode("utf-8")
-
         file_path = f"{config.REPO_DIRECTORY_PATH}/{uploaded_file.name}"
 
         try:
-            existing_file = repo.get_contents(file_path, ref=config.REPO_BRANCH)  # noqa: F841
-            container.warning(
-                f"El documento '{uploaded_file.name}' ya existe. Omitiendo...", icon="⚠️"
-            )
-            cprint(
-                f"El documento '{uploaded_file.name}' ya existe. Omitiendo...", "yellow"
-            )
-            time.sleep(2)
-            continue
+            repo.get_contents(file_path, ref=config.REPO_BRANCH)
+            message = f"El documento '{uploaded_file.name}' ya existe. Omitiendo..."
+            cprint(message, "yellow")
         except GithubException as e:
-            if e.status != 404:
-                container.error(
-                    f"Error al obtener el documento '{uploaded_file.name}': {e}",
-                    icon="❌",
-                )
-                cprint(
-                    f"Error al obtener el documento '{uploaded_file.name}': {e}",
-                    "red",
-                )
-                continue
+            if e.status == 404:
+                try:
+                    message = f"Documento '{uploaded_file.name}' añadido."
+                    repo.create_file(
+                        path=file_path,
+                        message=message,
+                        content=content,
+                        branch=config.REPO_BRANCH,
+                    )
+                    cprint(message, "green")
+                except GithubException as e:
+                    message = f"Error al añadir el documento '{uploaded_file.name}'"
+                    cprint(f"{message} : {e}", "red")
+            else:
+                message = f"Error al obtener el documento '{uploaded_file.name}'"
+                cprint(f"{message} : {e}", "red")
 
-        try:
-            message = f"Documento '{uploaded_file.name}' añadido."
-            repo.create_file(
-                path=file_path,
-                message=message,
-                content=content,
-                branch=config.REPO_BRANCH,
-            )
-            container.success(
-                f"Documento '{uploaded_file.name}' añadido exitosamente.", icon="✅"
-            )
-            cprint(f"Documento '{uploaded_file.name}' añadido exitosamente.", "green")
-            time.sleep(2)
-        except GithubException as e:
-            container.error(f"Error al añadir el documento '{uploaded_file.name}': {e}")
-            cprint(f"Error al añadir el documento '{uploaded_file.name}': {e}", "red")
+        progress_bar.progress((i + 1) / len(file_list), text=message)
+    time.sleep(1)
+    progress_bar.progress(1.0, text="Todos los documentos añadidos.")
+    time.sleep(1)
 
 
-def upload_repo_docs(namespace: str = "Reglamentos"):
+def upload_repo_docs(namespace: str = "Reglamentos") -> None:
     """
     Carga documentos de un repositorio Git, los divide en fragmentos y los añade a un almacenamiento de vectores.
 
