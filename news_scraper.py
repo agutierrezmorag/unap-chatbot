ee2d6c4cee54ff60a360cfb8ab2357da2bee0ac8
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-02-09 21:12:33 -0300

    Remove weekly news update workflow and news scraper script

--- news_scraper.py
+++
@@ -1,8 +0,0 @@
-from doc_manager.pinecone_management import process_and_load_documents
-from utils import config
-
-process_and_load_documents(
-    "news",
-    "Noticias",
-    "https://www.unap.cl/prontus_unap/site/edic/base/port/actualidad.html",
-)
