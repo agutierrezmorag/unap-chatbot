8ac3f2760bc9077e2179ccfc5e2f69c373a179c5
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-10-17 14:33:59 -0300

    Refactor create_account.py to support translated form fields during user registration

--- app_pages/create_account.py
+++ app_pages/create_account.py
@@ -7,7 +7,18 @@ if __name__ == "__page__":
             email_of_registered_user,
             username_of_registered_user,
             name_of_registered_user,
-        ) = st.session_state.authenticator.register_user()
+        ) = st.session_state.authenticator.register_user(
+            fields={
+                "Form name": "Registro de usuario",
+                "Email": "Email",
+                "Username": "Nombre de usuario",
+                "Password": "Contraseña",
+                "Repeat password": "Confirmar contraseña",
+                "Password hint": "Pista de contraseña",
+                "Captcha": "Captcha",
+                "Register": "Crear cuenta",
+            }
+        )
         if email_of_registered_user:
             with open(".streamlit/auth_config.yaml", "w") as file:
                 yaml.dump(st.session_state.auth_config, file, default_flow_style=False)
